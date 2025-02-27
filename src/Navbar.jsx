894b9a69f24246d9377b9cbdcb3e9e23fa4beb00
hengersorweb <bencze.istvan@hengersor.hu>
2025-02-27 11:54:50 +0100

    Értékelés

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = (() =>{
     return(
         <div>
             <nav className="bt bb tc mw7 center mt4">
-                <Link className="f6 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" to='/'>Fooldal</Link>
-                <NavLink className="f6 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" to='/ermesek'>Az olimpia ermesei</NavLink>
+                <Link className="f6 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" to='/'>Főoldal</Link>
+                <NavLink className="f6 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" to='/ermesek'>Pilipócs</NavLink>
                 <NavLink className="f6 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" to='/mermesek'>A magyar ermesek</NavLink>
             </nav>
 
